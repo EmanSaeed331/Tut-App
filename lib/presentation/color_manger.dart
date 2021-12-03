cc1e528baa01dc2aca360ce882601011bb3f8570
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-03 14:43:04 +0200

    appBar theme added

--- lib/presentation/color_manger.dart
+++ lib/presentation/color_manger.dart
@@ -4,21 +4,21 @@ import 'package:flutter/material.dart';
 class ColorsManger {
 
 
-  static Color? primaryColor =HexColor.fromHex('#ED9728');
-  static Color? darkGrey = HexColor.fromHex('#ED9728');
-  static Color? grey =HexColor.fromHex('#ED9728');
-  static Color? lightgrey = HexColor.fromHex('#ED9728');
-  static Color? primaryOpacity70= HexColor.fromHex('#ED9728');
+  static Color primaryColor =HexColor.fromHex('#ED9728');
+  static Color darkGrey = HexColor.fromHex('#ED9728');
+  static Color grey =HexColor.fromHex('#ED9728');
+  static Color lightgrey = HexColor.fromHex('#ED9728');
+  static Color primaryOpacity70= HexColor.fromHex('#ED9728');
 
-  static Color? darkPrimary =HexColor.fromHex('#d17d11');
-  static Color? grey1 = HexColor.fromHex('#707070');
-  static Color? grey2 =HexColor.fromHex('#797979');
-  static Color? white = HexColor.fromHex('#FFFFFF');
-  static Color? error= HexColor.fromHex('#e61f34');
+  static Color darkPrimary =HexColor.fromHex('#d17d11');
+  static Color grey1 = HexColor.fromHex('#707070');
+  static Color grey2 =HexColor.fromHex('#797979');
+  static Color white = HexColor.fromHex('#FFFFFF');
+  static Color error= HexColor.fromHex('#e61f34');
 
 }
 extension HexColor on Color {
-  static Color? fromHex(String hexColorString){
+  static Color fromHex(String hexColorString){
     hexColorString = hexColorString.replaceAll('#', '');
     if(hexColorString.length==6){
       hexColorString= 'FF'+hexColorString;
--- lib/presentation/theme_manger.dart
+++ lib/presentation/theme_manger.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tutapp/presentation/color_manger.dart';
+import 'package:tutapp/presentation/font_manger.dart';
+import 'package:tutapp/presentation/style_manger.dart';
 import 'package:tutapp/presentation/value_manger.dart';
 ThemeData getApplicatoinTheme (){
   return ThemeData(
@@ -9,11 +11,20 @@ ThemeData getApplicatoinTheme (){
     primaryColorDark:ColorsManger.darkPrimary,
     disabledColor: ColorsManger.grey1,
     accentColor:ColorsManger.grey ,
+    //ripple color
+    splashColor: ColorsManger.primaryOpacity70,
     //card vieew theme
     cardTheme: CardTheme(
       color : ColorsManger.white,
       shadowColor: ColorsManger.grey,
       elevation: AppSize.s4,
+    ),
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color:ColorsManger.primaryColor,
+      elevation: AppSize.s4,
+      shadowColor: ColorsManger.primaryOpacity70,
+      titleTextStyle: getRegularStyle(color:ColorsManger.white, fontSize: FontSize.s16),
     ),
 
   );
